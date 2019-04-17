6562448a36db93a006c1a48648a1759067649162
luo-xiao <luo-xiao@outlook.com>
2019-04-17 21:44:14 +0200

    calculate mean/median/mode/weighted mean elegantly

--- 4_statistics.py
+++ 4_statistics.py
@@ -11,9 +11,12 @@ median = np.median(data1)
 mode = stats.mode(data1)   # the most frequent item (or the smallest item if multi-modal)
 
 # weighted mean
-data2 = data1
+data2 = [23, 34, 56, 12, 43, 23, 43]
 weight = '1 2 3 4 5'
 
 w = list(map(int, weight.strip().split()))  # map: use function for each element in a list
 weighted_mean = sum([a*b for a, b in zip(data2, w)]) / sum(w)   # formula
 print(format(weighted_mean, '.1f'))         # output with 1 decimal
+
+
+#
