1815348c0fce09be09641e2de56bcdecec2c97e0
luo-xiao <luo-xiao@outlook.com>
2019-05-18 13:25:13 +0200

    reorganize

---
+++ 1_data_stucture/list.py
@@ -0,0 +1,8 @@
+
+
+# count number 4 in a list
+l = [1,2,3,4,5,6,7,8,5,3,4]
+print('number 4 appears ' + str(l.count(4)) + ' times.')
+
+# concatenate elements into a string
+print('list: ' + ' '.join(list(map(lambda x: str(x),l))))
---
+++ 1_data_stucture/set.py
@@ -0,0 +1,21 @@
+
+
+# python3x
+a = set(["White", "Black", "Red"])
+b = set(["Red", "Green"])
+
+print('In a but not in b:')
+print(a-b)
+print('\n')
+
+print('In a or b or both:')
+print(a | b)
+print('\n')
+
+print('In both a and b:')
+print(a & b)
+print('\n')
+
+print('in a or b but not bote:')
+print(a ^ b)
+print('\n')
